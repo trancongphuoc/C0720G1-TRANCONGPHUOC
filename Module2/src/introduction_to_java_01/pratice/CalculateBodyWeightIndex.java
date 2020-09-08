75b02135a3a9ef502e2c1632140d333f09540462
tran cong phuoc <teosake.1999@gmail.com>
2020-09-08 17:37:47 +0700

    08-9

---
+++ Module2/src/introduction_to_java_01/pratice/CalculateBodyWeightIndex.java
@@ -0,0 +1,26 @@
+package introduction_to_java_01.pratice;
+
+import java.util.Scanner;
+
+public class CalculateBodyWeightIndex {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input weight: ");
+        double weight = scanner.nextDouble();
+        System.out.println("Input height: ");
+        double height = scanner.nextDouble();
+        double bmi;
+
+        bmi = weight / Math.pow(height, 2);
+
+        if (bmi < 18.5) {
+            System.out.println("Underweight.");
+        } else if (bmi >= 18.5 && bmi < 25.0) {
+            System.out.println("Normal.");
+        } else if (bmi >= 25.0 && bmi <30.0) {
+            System.out.println("Overweight");
+        } else if (bmi >= 30.0) {
+            System.out.println("Obese");
+        }
+    }
+}
---
+++ Module2/src/introduction_to_java_01/pratice/DisplaySystemTime.java
@@ -0,0 +1,10 @@
+package introduction_to_java_01.pratice;
+
+import java.util.Date;
+
+public class DisplaySystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Date now: " + now);
+    }
+}
