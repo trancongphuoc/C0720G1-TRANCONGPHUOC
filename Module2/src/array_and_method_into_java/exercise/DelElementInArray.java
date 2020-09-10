3af2505c8aec8f2c31edcf79e65f35785533f802
tran cong phuoc <teosake.1999@gmail.com>
2020-09-10 09:17:33 +0700

    ---

--- Module2/src/array_and_method_into_java/exercise/DelElementInArray.java
+++ Module2/src/array_and_method_into_java/exercise/DelElementInArray.java
@@ -5,19 +5,28 @@ import java.util.Scanner;
 public class DelElementInArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] array = {1,5,3,6,9,4,5};
+        int[] array = {1, 5, 3, 6, 9, 4, 5};
         int[] array1 = new int[array.length];
         System.out.println("Enter element need del: ");
         int x = scanner.nextInt();
+        int count = 0;
 
+        //Xóa phần tử
         for (int i = 0, j = 0; i < array.length; i++) {
-            if (array[i] != x) {
-                array1[j] = array[i];
-                j++;
+            if (array[i] == x) {
+                count++;
+                continue;
             }
+            array[j] = array[i];
+            j++;
+        }
+        
+        //Gán các phần tử bị xóa = 0 vào cuối mảng
+        for (int i = array.length - 1; i > array.length - count - 1; i--) {
+            array[i] = 0;
         }
 
-        for (int i : array1) {
+        for (int i : array) {
             System.out.print(i + "\t");
         }
     }
