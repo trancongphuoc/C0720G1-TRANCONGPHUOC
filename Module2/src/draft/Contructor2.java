d074a94fcbbdb1622ba087f119d8574b28d6f9fb
tran cong phuoc <teosake.1999@gmail.com>
2020-10-19 09:14:14 +0700

    ---

---
+++ Module2/src/draft/Contructor2.java
@@ -0,0 +1,16 @@
+package draft;
+
+import java.util.LinkedList;
+
+public class Person {
+    Person() {
+        System.out.println("Hello");
+        return;
+    }
+}
+
+class Contructor2 {
+    public static void main(String[] args) {
+        Person p = new Person();
+    }
+}
